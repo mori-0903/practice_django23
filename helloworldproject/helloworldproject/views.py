12c1de18e705fd1a4201e1ad8ccca6805e8dfc50
R.moriyama <moriforest537@gmail.com>
2021-06-17 21:46:46 +0900

    wip

--- helloworldproject/helloworldproject/views.py
+++ helloworldproject/helloworldproject/views.py
@@ -1,6 +1,11 @@
 from django.http.response import HttpResponse
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def helloworldfuction(request):
     returnedobject =  HttpResponse('<h1>hello world</h1>')
     return returnedobject
+
+class Helloworldclass(TemplateView):
+    template_name = 'hello.html'
+
